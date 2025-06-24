76d29432fe0b98dd539c28370b34b72b422a1b78
misbahx <misbahx.id@gmail.com>
2025-06-24 17:48:06 +0700

    feat: create students migration

---
+++ database/migrations/2025_06_24_103559_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis')->unique();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('kelas');
+            $table->string('tahun_masuk');
+            $table->string('ttl');
+            $table->string('alamat');
+            $table->string('kontak_ortu')->unique();
+            $table->enum('status', ['aktif', 'tidak_aktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
