6e9fbb9c8530d9e9df546bcc1be7ef7c5d9eb19a
misbahlmnr <misbahx.id@gmail.com>
2025-08-28 10:01:35 +0700

    feat: custom button edit/view/delete useDataTables

--- resources/js/composables/useDataTables.ts
+++ resources/js/composables/useDataTables.ts
@@ -39,15 +39,15 @@ export function useDataTable(
                           data: 'id',
                           orderable: false,
                           searchable: false,
-                          className: 'flex items-center justify-center gap-1',
+                          className: 'flex items-center justify-center gap-2',
                           render: (data: any, type: any, row: any) => {
                               if (actions) {
                                   return actions(data, row);
                               }
                               return `
-                                    ${editRoute ? `<button class="btn-edit text-blue-500 cursor-pointer" data-id="${data}">Edit</button> |` : ''}
-                                    ${detailRoute ? `<button class="btn-detail text-green-500 cursor-pointer" data-id="${data}">Detail</button> |` : ''}
-                                    ${deleteRoute ? `<button class="btn-delete text-red-500 cursor-pointer" data-id="${data}">Hapus</button>` : ''}
+                                    ${editRoute ? `<button class="btn-edit text-yellow-500 btn-custom-outline" data-id="${data}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg></button>` : ''}
+                                    ${detailRoute ? `<button class="btn-detail text-green-500 cursor-pointer btn-custom-outline" data-id="${data}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg></button>` : ''}
+                                    ${deleteRoute ? `<button class="btn-delete text-red-500 cursor-pointer btn-custom-outline" data-id="${data}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg></button>` : ''}
                               `;
                           },
                       },
